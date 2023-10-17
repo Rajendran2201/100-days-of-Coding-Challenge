0d00463a53145245d0fed0df0f5014e6d22a7bfc
RAJENDRAN <137254223+Rajendran2201@users.noreply.github.com>
2023-10-17 18:26:21 +0530

    Update employeeDetails.java

--- Week_1/Day 3/employeeDetails.java
+++ Week_1/Day 3/employeeDetails.java
@@ -1,3 +1,8 @@
+/*
+    Create a class Employee to store the details of the employees and methods to insert and print the data 
+    The data/details to be fetched are id,name,age,mobile-no,address
+*/
+
 public class employeeDetails {
     public static void main(String[] args) {
         Employee e = new Employee();
