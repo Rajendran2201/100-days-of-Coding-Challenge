d2f4972e93c49d509ebcdb3891ad009aa951ce11
RAJENDRAN <137254223+Rajendran2201@users.noreply.github.com>
2023-10-17 18:41:40 +0530

    Update shapes.java

--- Week_1/Day 3/shapes.java
+++ Week_1/Day 3/shapes.java
@@ -1,3 +1,9 @@
+/*
+    Creating a class box inorder to calculate the area and volume of a shape which has three dimensions (width,height,breadth) 
+    The area nd volume can be calculated by using two different methods namely areaOf and volumeOf
+    
+*/
+
 public class shapes {
     public static void main(String[] args) {
         Box b = new Box(2,3,4);
