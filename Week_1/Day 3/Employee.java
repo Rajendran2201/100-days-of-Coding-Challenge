576f7956bd84526ee43b41219c2284586de14289
RAJENDRAN <137254223+Rajendran2201@users.noreply.github.com>
2023-10-17 18:23:09 +0530

    Add files via upload

---
+++ Week_1/Day 3/Employee.java
@@ -0,0 +1,21 @@
+public class Employee {   
+    int id;
+    String name;
+    int age;
+    String MobNo;
+    String address;
+    void insertvalues(int i ,String n ,int a,String m,String ad){
+        id = i;
+        name = n;
+        age = a;
+        MobNo = m;
+        address = ad;
+    }
+    void  printvalues(){
+        System.out.print("id : "+id+
+        "\nname : "+name+
+        "\nage : "+age+
+        "\nMobile No : "+MobNo+
+        "\nAddress : "+address);
+    }
+}
---
+++ Week_1/Day 3/employeeDetails.java
@@ -0,0 +1,7 @@
+public class employeeDetails {
+    public static void main(String[] args) {
+        Employee e = new Employee();
+        e.insertvalues(1,"Raj",18,"9876543210","Coimbatore");
+        e.printvalues();
+    }
+}
