038e776e4a24c83cb49ba10141649bc915fee26d
RAJENDRAN <137254223+Rajendran2201@users.noreply.github.com>
2023-10-18 23:18:24 +0530

    Update SuperMarket.java

--- Week_1/Day 4/SuperMarket.java
+++ Week_1/Day 4/SuperMarket.java
@@ -1,6 +1,12 @@
 public class SuperMarket {
 
-    //Inheritance Concept is used here 
+    /*Inheritance Concept is used inorder to access the data from multiple classes
+    here , an object for the class Price is created whereas price is the subclass that has been derived from the sub class AddData 
+    the variables item and quantity are declared in the class "AddData" and the variable price is declared in the class "Price" 
+    yet, the Subclass/Derived class Price can acces all the three variables. This is the power of Inheritance. 
+    Such that the significant property of Inheritance is code reusability 
+
+*/
     public static void main(String[] args) {
         Price a = new Price("Pencil");
         System.out.println("The item bought is "+a.item);
