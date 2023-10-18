50e6257f91e4290c01a8f716c7663061d4a9b292
RAJENDRAN <137254223+Rajendran2201@users.noreply.github.com>
2023-10-18 23:20:49 +0530

    Update Price.java

--- Week_1/Day 4/Price.java
+++ Week_1/Day 4/Price.java
@@ -1,7 +1,12 @@
+// the Class Price is the subclass / Derived class that has been extended from the base class "AddData"
+// This class can access the data from the base class a it is extended from it.
+
+// code of DERIVED CLASS 
+
 public class Price extends AddData{
     
     Price(String item) {
-        super(item);
+        super(item);                //Super() has to be used mandatorily inside the constructor 
     }
 
     int price = 1000;
