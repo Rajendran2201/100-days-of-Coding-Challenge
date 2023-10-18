22a29e34f9966be92e5af20d0caf305f27d3628e
RAJENDRAN <137254223+Rajendran2201@users.noreply.github.com>
2023-10-18 23:25:17 +0530

    Update AddData.java

--- Week_1/Day 4/AddData.java
+++ Week_1/Day 4/AddData.java
@@ -1,3 +1,16 @@
+/*
+    This is the base class that has been used by the sub class / derived class . 
+    TO acces the data from this class (AddData) , we can just extend it in the new class (Price)
+    Such the variables declared within the scope of AddData class can be used in the sub-class as  well
+
+
+            Point to be noted : The base class is not used inside the main function . 
+            We use the derived class inside the main function as it can acces the data of the base class
+
+        
+    CODE FOR BASE CLASS 
+*/
+
 public class AddData {
 
     String item;
