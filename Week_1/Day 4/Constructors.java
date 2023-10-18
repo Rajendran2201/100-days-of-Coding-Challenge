eb408c6acbeda86ab7115e13ec850a9ed81ce284
RAJENDRAN <137254223+Rajendran2201@users.noreply.github.com>
2023-10-18 23:00:38 +0530

    Update Constructors.java

--- Week_1/Day 4/Constructors.java
+++ Week_1/Day 4/Constructors.java
@@ -1,9 +1,18 @@
+/*
+    Working with Constructors 
+
+    Constructors are called when they objected are created ! 
+    A constructor should have the same name as the class.
+    The constructors are called when objects c1, c2 and c3 are created 
+
+*/
+
 package Constructors;
 public class Constructors {
     public static void main(String[] args){
-        Construct c1 = new Construct();
-        Construct c2 = new Construct(8);
-        Construct c3 = new Construct("Raj", 18);
+        Construct c1 = new Construct();                     //Construct() is called 
+        Construct c2 = new Construct(8);                    // Construct(int val) is called 
+        Construct c3 = new Construct("Raj", 18);            // Construct(String name, int age) is called
     }
 }
 
