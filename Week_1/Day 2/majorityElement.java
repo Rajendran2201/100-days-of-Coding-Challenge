dc1e8e39fefd0c2f780bf2a6db3aba388cbc5783
RAJENDRAN <137254223+Rajendran2201@users.noreply.github.com>
2023-10-18 18:59:41 +0530

    Update majorityElement.java

--- Week_1/Day 2/majorityElement.java
+++ Week_1/Day 2/majorityElement.java
@@ -1,8 +1,7 @@
 import java.util.HashMap;
 
-public class majorityElement {
-    /*
-     * 169. MAJORITY ELEMENT
+  /*
+     * 169. MAJORITY ELEMENT      ( LEETCODE )
      * 
      * Given an array nums of size n, return the majority element
      * The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
@@ -10,6 +9,10 @@ public class majorityElement {
      * Example 1 : Input: nums = [3,2,3]                Output: 3
      * Example 2 : Input: nums = [2,2,1,1,1,2,2]        Output: 2
      */
+
+
+public class majorityElement {
+  
     public static void main(String[] args){
         int[] nums = {3,2,3};
         int majority = majorityElement(nums);
