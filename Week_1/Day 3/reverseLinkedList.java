8b4f2b882280a6369c5ed50ac85da2f04197f02b
RAJENDRAN <137254223+Rajendran2201@users.noreply.github.com>
2023-10-18 19:00:33 +0530

    Update reverseLinkedList.java

--- Week_1/Day 3/reverseLinkedList.java
+++ Week_1/Day 3/reverseLinkedList.java
@@ -1,6 +1,6 @@
 import java.util.*;
 /*
- * 206. REVERSE LINKED LIST 
+ * 206. REVERSE LINKED LIST       ( LEETCODE )
  * 
  * Given the head of a singly linked list, reverse the list, and return the reversed list.
  * Example: Input: head = [1,2,3,4,5]                       Output: [5,4,3,2,1]
