32009ed44368fbafdbea5628cd3017671cbd111f
RAJENDRAN <137254223+Rajendran2201@users.noreply.github.com>
2023-10-18 18:59:09 +0530

    Update intersectionOfTwoArrays.java

--- Week_1/Day 2/intersectionOfTwoArrays.java
+++ Week_1/Day 2/intersectionOfTwoArrays.java
@@ -3,15 +3,18 @@ import java.util.HashMap;
 import java.util.Set;        //Here We have used HashSet inorder to ensure the uniqueness of the elements 
 import java.util.HashSet;
 
-public class intersectionOfTwoArrays {
-    /*349. INTERSECTION OF TWO ARRAYS
+  /*349. INTERSECTION OF TWO ARRAYS    ( LEETCODE )
      * Given two integer arrays nums1 and nums2, return an array of their intersection. 
      * Each element in the result must be unique and you may return the result in any order.
      * 
      * Example 1 : Input: nums1 = [1,2,2,1], nums2 = [2,2]      Output: [2]
      * Example 2 : Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]  Output: [9,4]
                                                                 Explanation: [4,9] is also accepted.
-     */ 
+     */
+
+
+public class intersectionOfTwoArrays {
+   
     public static void main(String[] args){
         int[] nums1 = {4,9,5};
         int[] nums2 = {9,4,9,8,4};
