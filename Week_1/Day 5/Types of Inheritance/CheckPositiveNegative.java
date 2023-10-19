495bf00a382a63b4d8cb30b6f8d710a54aaee419
RAJENDRAN <137254223+Rajendran2201@users.noreply.github.com>
2023-10-19 06:00:37 +0530

    Update CheckPositiveNegative.java

--- Week_1/Day 5/Types of Inheritance/CheckPositiveNegative.java
+++ Week_1/Day 5/Types of Inheritance/CheckPositiveNegative.java
@@ -1,9 +1,19 @@
-public class CheckPositiveNegative extends CheckOddEven {
+/*
+It is sub class which has been derived from the base class "CheckOddEven"
+It evaluates whether the given number is POsitive or negative upon the creation of the object. 
+This is because the code is written inside the constructor.
+
+Since CheckPositiveNegative is the derived class, It can access the method "OddOrEven" of the parent class which evaluates whether the given number is Odd or Even 
+
+*/
+
+
+public class CheckPositiveNegative extends CheckOddEven {        // Derived_class extends Base_class
     boolean oddEven;
 
     CheckPositiveNegative(int value){
-        super();                                //super() must be the first statement in a constructor 
-        oddEven = OddOrEven(value);
+        super();                                                  //super() must be the first statement in a constructor 
+        oddEven = OddOrEven(value);                               //This method is declared in the parent class
         if(value < 0){
             if(oddEven){
                 System.out.println("It is a Negative Even NUmber");
