48e9ef819d7b47b94c3e78e92862385cd729197c
RAJENDRAN <137254223+Rajendran2201@users.noreply.github.com>
2023-10-19 06:04:14 +0530

    Update CheckOddEven.java

--- Week_1/Day 5/Types of Inheritance/CheckOddEven.java
+++ Week_1/Day 5/Types of Inheritance/CheckOddEven.java
@@ -1,17 +1,27 @@
+/*
+    It is the parent class which conatins the method "CheckOddEven" that evaluates whether the given number is odd or even .
+    This method is used by the derived class.
+    The code written here can be accessed by the class "CheckPositiveNegative" as it has been extended from here.
+
+    PARENT CLASS 
+    
+*/
+
+
 public class CheckOddEven{
 
     int value;
     
-    public CheckOddEven() {
+    public CheckOddEven() {                                //Constructor with no parameters 
         super();
     }
 
-    CheckOddEven(int value){
+    CheckOddEven(int value){                                //Constructor with a parameter
         super();
         this.value = value;  
     }
 
-    boolean OddOrEven(int value){
+    boolean OddOrEven(int value){                            //Method to evaluate whether the value provided is odd or even 
         if(value % 2==0){
             return true;
         }else{
