c79df48a1668e2318858fc363836266dd73c0ee3
RAJENDRAN <137254223+Rajendran2201@users.noreply.github.com>
2023-10-19 06:21:23 +0530

    Update MultilevelInheritance.java

--- Week_1/Day 5/Types of Inheritance/MultilevelInheritance.java
+++ Week_1/Day 5/Types of Inheritance/MultilevelInheritance.java
@@ -1,3 +1,14 @@
+/*
+            Thw Structure of Multilevel Inheritance A -> B -> C
+            Here there will be 
+                1)grandParent class - fetch name
+                2) parent class - fetch age
+                3) derived class - fetch mark
+
+Point to be noted : In java, Class cannot acces the grandParent's members
+
+*/
+
 import java.util.Scanner;
 
 public class MultilevelInheritance {
@@ -7,9 +18,9 @@ public class MultilevelInheritance {
         System.out.print("Enter Your Roll No :");
         rollno = sc.nextInt();
 
-        FetchMark f = new FetchMark();
-        f.printName(rollno);
-        f.printAge(rollno);
-        f.printMark(rollno);
+        FetchMark f = new FetchMark();            // The derived class can acces the methods of the parent and grandParent classes
+        f.printName(rollno);                        //method in grandparent class
+        f.printAge(rollno);                         //method in parent class
+        f.printMark(rollno);                        //method in Derived class
     }
 }
