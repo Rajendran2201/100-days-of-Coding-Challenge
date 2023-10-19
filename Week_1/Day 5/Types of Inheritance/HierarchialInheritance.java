f20693bfec79827894c9cca9ebcc70f72686d9e4
RAJENDRAN <137254223+Rajendran2201@users.noreply.github.com>
2023-10-19 16:48:07 +0530

    Update HierarchialInheritance.java

--- Week_1/Day 5/Types of Inheritance/HierarchialInheritance.java
+++ Week_1/Day 5/Types of Inheritance/HierarchialInheritance.java
@@ -1,3 +1,12 @@
+/**
+
+This is an example of Heirarchial Inheritance 
+MultiplyData is the base class whereas AddData and SubtractData are the sub class derived from the parent class
+Thus the constructor of the parent class is called when the object is created for the sub classes
+
+
+*/
+
 public class HierarchialInheritance{
 public static void main(String[] args) {
     AddData a = new AddData(2,3,4);                     // the parent class constructor and the constructor of this class are executed 
