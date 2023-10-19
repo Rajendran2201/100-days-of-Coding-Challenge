2e6b0a5cc74cb36145362b9eeb412378091a83e7
RAJENDRAN <137254223+Rajendran2201@users.noreply.github.com>
2023-10-19 05:55:52 +0530

    Update SingleInheritance.java

--- Week_1/Day 5/Types of Inheritance/SingleInheritance.java
+++ Week_1/Day 5/Types of Inheritance/SingleInheritance.java
@@ -1,3 +1,16 @@
+/*
+ Java Program to find whether the given number is positive or Negative along with Odd or Even uing Single Inheritance 
+
+ 1) Base Class - CheckOddEven which contains a method named "OddOrEven" which evaluates whether the number is odd or even
+ 2) Derived Class - CheckPositiveNegative it checks whether the given number is POsitive or Negative immediately on the 
+ creation of Object as the code is written within the Constructor.
+
+ The Constructor of the derived class uses the method (OddOrEven) from its parent class. 
+ When a subclass is derived from a parent class, It is said to be Single Inheritance 
+
+
+*/
+
 import java.util.Scanner;
 public class SingleInheritance{
     public static void main(String[] args) {
