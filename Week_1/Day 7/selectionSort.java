e8afa7c399d8855fb958cb83f20c442b1faacbc4
RAJENDRAN <137254223+Rajendran2201@users.noreply.github.com>
2023-10-21 23:11:26 +0530

    Update selectionSort.java

--- Week_1/Day 7/selectionSort.java
+++ Week_1/Day 7/selectionSort.java
@@ -1,15 +1,24 @@
+/*
+        CODE FOR SELECTION SORT 
+
+        * It is a sorting technique which takes a time complexity of O(n^2) as it has to traverse the array with two loops 
+
+        
+*/
+
+
 public class selectionSort {
 
     static void sort(int[] arr){
        
-        for(int i=0;i<arr.length-1;i++){
+        for(int i=0;i<arr.length-1;i++){    
             int min = i ;
-            for(int j=i+1;j<arr.length;j++){
+            for(int j=i+1;j<arr.length;j++){    //This loop is to find the smallest element in the unsorted array
                 if(arr[j]<arr[min]){
                     min = j;
                 }
             }
-            int temp = arr[i];
+            int temp = arr[i];    //Swapping of the minimum value in its respective place
             arr[i] = arr[min];
             arr[min] = temp;
         }
