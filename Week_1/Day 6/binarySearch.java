673a3ac69b9db50bde2fcf814c142b20367512a0
RAJENDRAN <137254223+Rajendran2201@users.noreply.github.com>
2023-10-21 22:15:21 +0530

    Update binarySearch.java

--- Week_1/Day 6/binarySearch.java
+++ Week_1/Day 6/binarySearch.java
@@ -1,3 +1,26 @@
+/*        CODE FOR BINARY SEARCH 
+
+Binary Search is defined as a searching algorithm used in a sorted array by repeatedly dividing the search interval in half. 
+The idea of binary search is to use the information that the array is sorted and reduce the time complexity to O(log N). 
+
+In this algorithm, 
+
+    * Divide the search space into two halves by finding the middle index “mid”. 
+    * Compare the middle element of the search space with the key/target. 
+    * If the key is found at middle element, the process is terminated.
+    * If the key is not found at middle element, choose which half will be used as the next search space.
+        * If the key is smaller than the middle element, then the left side is used for next search.
+        * If the key is larger than the middle element, then the right side is used for next search.
+    * This process is continued until the key is found or the total search space is exhausted.
+
+
+
+* The Binary search algorithm takes O(log n) time complexity inorder to search for an element in an array
+* It basically uses the "Divide and Conquer" approach
+
+*/
+
+
 import java.util.Scanner;
 
 public class binarySearch {
