e99ddf6dd4e18afd2de917d40f0eb0af46e34d35
RAJENDRAN <137254223+Rajendran2201@users.noreply.github.com>
2023-10-21 22:44:59 +0530

    Update bubbleSort.java

--- Week_1/Day 7/bubbleSort.java
+++ Week_1/Day 7/bubbleSort.java
@@ -1,3 +1,13 @@
+/*
+                CODE FOR BUBBLE SORT 
+
+    Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in the wrong order. 
+    This algorithm is not suitable for large data sets as its average and worst-case time complexity is quite high.
+
+    * It has a time complexity of O(n^2) runtime as it has to traverse the array with two loops
+
+*/
+
 public class bubbleSort{
     static void sort(int[] arr){
          for(int i=0;i<arr.length;i++){
