2c55e855479d656bf3939533dc9192a09320d6b9
RAJENDRAN <137254223+Rajendran2201@users.noreply.github.com>
2023-10-21 21:39:05 +0530

    Update linearSearch.java

--- Week_1/Day 6/linearSearch.java
+++ Week_1/Day 6/linearSearch.java
@@ -1,3 +1,24 @@
+/*         CODE FOR LINEAR SEARCH IN ARRAY
+
+Problem: Given an array arr[] of n elements, write a function to search a given element x in arr[]. 
+
+ALGORITHM :
+Step 1: Start 
+Step 2: Declare an array and search element as key.
+Step 3: Traverse the array until the number is found.
+Step 4: If the key element is found, return the index position of the array element
+Step 5: If the key element is not found, return -1
+Step 6: Stop.
+ 
+
+* Linear search is a type of searching technique which is used to find a specific element in an array 
+* It requires O(n) time complexity 
+* here , we basically use a for loop inorder to iterate through each element in the array and check if it matches with the target provided
+* If the element is matched with the target , then the index of the element is returned.
+* If no element in the array is matched with the target , then -1 is returned 
+
+
+    */
 import java.util.Scanner;
 public class linearSearch{
     static int search(int[] arr, int target){
