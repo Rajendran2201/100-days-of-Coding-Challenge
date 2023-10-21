052dca7f614091d67264c37d75ebdf9a41384e9b
RAJENDRAN <137254223+Rajendran2201@users.noreply.github.com>
2023-10-21 22:51:26 +0530

    Update insertionSort.java

--- Week_1/Day 7/insertionSort.java
+++ Week_1/Day 7/insertionSort.java
@@ -1,3 +1,13 @@
+/*
+        CODE FOR INSERTION SORT
+
+Insertion sort is a simple sorting algorithm that works similar to the way you sort playing cards in your hands. 
+The array is virtually split into a sorted and an unsorted part. 
+Values from the unsorted part are picked and placed at the correct position in the sorted part.
+
+*/
+
+
 public class insertionSort {
     public static void main(String[] args) {
         int[] arr = {8,9,7,6,4,5,3,1,2};
@@ -11,14 +21,21 @@ public class insertionSort {
 
     private static void sort(int[] arr) {
 
-        for(int i=1;i<arr.length;i++){
-            int key = arr[i];
-            int j= i-1;
-            while(j>=0 && arr[j]>key){
+        for(int i=1;i<arr.length;i++){ 
+            //we assume that the first element of the array is already sorted such that we start out traversal from second element
+
+            int key = arr[i]; // We are taking a duplicate of out current element
+            int j= i-1; // j represent the index of the last element of the sorted array
+            
+            while(j>=0 && arr[j]>key){ 
+                // The first condition ensures that the pointer doesn't go to negative values such that array begins with the initial index of zero
+                // The second condition checkas whether the current element is smaller than the element in the sorted array,
+                        // If yes , then elements in the sorted array are shifted towards rightside once and this process is continued until the condition returns false
+                
                 arr[j+1] = arr[j];
                 j--;
             }
-            arr[j+1] = key;
+            arr[j+1] = key;        // Finally our current element is placed in its respective position
         }
     }
 }
