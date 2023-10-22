e77edf57ab1b6ce6ec03937f9683443e640f3427
RAJENDRAN <137254223+Rajendran2201@users.noreply.github.com>
2023-10-22 10:36:12 +0530

    Update Solution_middleElement.java

--- Week_2/Day 8/Solution_middleElement.java
+++ Week_2/Day 8/Solution_middleElement.java
@@ -1,11 +1,18 @@
+/*
+    This is the leetcode solution for the problem 876.FIND THE MIDDLE ELEMENT 
+
+*/
+
 class Solution_middleElement {
     public ListNode middleNode(ListNode head) {
-        ListNode slow = head , fast = head;
+        ListNode slow = head , fast = head; //using the tortooise and the rabbit approach 
         while(fast!=null && fast.next!=null){
-            slow = slow.next;
-            fast = fast.next.next;
+            slow = slow.next;            //slow pointer moves by 1
+            fast = fast.next.next;        //fast pointer moves by 2
+
+            //such that when fast pointer reaches the end of the list , the slow pointer will be at the middle of the list
         }
-        return slow;
+        return slow;  // slow returns the list where the head will be the middle node 
     }
     
 }
