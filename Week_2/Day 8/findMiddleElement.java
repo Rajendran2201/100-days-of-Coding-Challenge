ca9a0bd42bed3c84d121c606c463c6aba9c028e7
RAJENDRAN <137254223+Rajendran2201@users.noreply.github.com>
2023-10-22 10:33:59 +0530

    876. FIND MIDDLE ELEMENT

--- Week_2/Day 8/findMiddleElement.java
+++ Week_2/Day 8/findMiddleElement.java
@@ -1,3 +1,20 @@
+/*
+        876. FIND MIDDLE ELEMENT         (LEETCODE)
+        
+        Given the head of a singly linked list, return the middle node of the linked list.
+
+        If there are two middle nodes, return the second middle node.
+
+Input: head = [1,2,3,4,5]
+Output: [3,4,5]
+Explanation: The middle node of the list is node 3.
+
+Input: head = [1,2,3,4,5,6]
+Output: [4,5,6]
+Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+
+*/
+
 public class findMiddleElement {
         public static void main(String[] args) {
             // Create a sample linked list: 1 -> 2 -> 3 -> 4 -> 5
@@ -6,8 +23,8 @@ public class findMiddleElement {
             // Initialize the Solution class
             Solution_middleElement solution = new Solution_middleElement();
     
-            // Find the middle node
-            ListNode middle = solution.middleNode(head);
+            // find the middle node
+            ListNode middle = solution.middleNode(head); 
     
             // Print the output: [3, 4, 5]
             System.out.print("Output: [");
