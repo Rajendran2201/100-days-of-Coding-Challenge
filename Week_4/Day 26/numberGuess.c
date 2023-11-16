fc27afcfc3db5bb90de770b9ae6df9da3b99a2b6
RAJENDRAN <137254223+Rajendran2201@users.noreply.github.com>
2023-11-16 20:49:19 +0530

    Update numberGuess.c

---
+++ Week_4/Day 26/numberGuess.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int number, guess, attempts = 0;
+    srand(time(0)); // Seed for random number generation
+    number = rand() % 100 + 1; // Generates a random number between 1 and 100
+    
+    printf("Welcome to the Number Guessing Game!\n");
+    
+    do {
+        printf("Enter your guess (between 1 and 100): ");
+        scanf("%d", &guess);
+        attempts++;
+        
+        if (guess > number) {
+            printf("Too high! Try again.\n");
+        } else if (guess < number) {
+            printf("Too low! Try again.\n");
+        } else {
+            printf("Congratulations! You guessed the number in %d attempts.\n", attempts);
+        }
+    } while (guess != number);
+    
+    return 0;
+}
